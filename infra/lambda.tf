58325f4d29bee55b31d25d70a2b41a32382b1785
Tan Le <tanlethanh8266@gmail.com>
2024-03-20 14:35:31 +0700

    chore: migrate new lambda runtime for go lambda function

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -23,18 +23,18 @@ data "archive_file" "translate" {
   depends_on = [null_resource.go_build]
 
   type        = "zip"
-  source_file = "../dist/translate"
+  source_dir  = "../dist/translate"
   output_path = "../dist/translate.zip"
 }
 
 resource "aws_lambda_function" "translate" {
   function_name    = "blinders-translate"
   filename         = "../dist/translate.zip"
-  handler          = "translate"
+  handler          = "bootstrap" # default for provided.al2
   role             = aws_iam_role.lambda_role.arn
+  runtime          = "provided.al2" # this runtime work with our built lambda (not provided.al2023)
+  architectures    = ["arm64"]
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-  memory_size      = 128
-  runtime          = "go1.x"
   source_code_hash = data.archive_file.translate.output_base64sha256
 
   environment {
@@ -48,17 +48,18 @@ data "archive_file" "connect" {
   depends_on = [null_resource.go_build]
 
   type        = "zip"
-  source_file = "../dist/connect"
+  source_dir  = "../dist/connect"
   output_path = "../dist/connect.zip"
 }
 
 resource "aws_lambda_function" "ws_connect" {
   function_name    = "blinders-ws-connect"
   filename         = "../dist/connect.zip"
-  handler          = "connect"
+  handler          = "bootstrap" # default for provided.al2
   role             = aws_iam_role.lambda_role.arn
+  runtime          = "provided.al2" # this runtime work with our built lambda (not provided.al2023)
+  architectures    = ["arm64"]
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-  runtime          = "go1.x"
   source_code_hash = data.archive_file.connect.output_base64sha256
 
   environment {
@@ -79,10 +80,11 @@ data "archive_file" "ws_authorizer" {
 resource "aws_lambda_function" "ws_authorizer" {
   function_name    = "blinders-ws-authorizer"
   filename         = "../dist/ws_authorizer.zip"
-  handler          = "handler"
+  handler          = "bootstrap" # default for provided.al2
   role             = aws_iam_role.lambda_role.arn
+  runtime          = "provided.al2" # this runtime work with our built lambda (not provided.al2023)
+  architectures    = ["arm64"]
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-  runtime          = "go1.x"
   source_code_hash = data.archive_file.ws_authorizer.output_base64sha256
 
   environment {
@@ -95,17 +97,18 @@ data "archive_file" "disconnect" {
   depends_on = [null_resource.go_build]
 
   type        = "zip"
-  source_file = "../dist/disconnect"
+  source_dir  = "../dist/disconnect"
   output_path = "../dist/disconnect.zip"
 }
 
 resource "aws_lambda_function" "ws_disconnect" {
   function_name    = "blinders-ws-disconnect"
   filename         = "../dist/disconnect.zip"
-  handler          = "disconnect"
+  handler          = "bootstrap" # default for provided.al2
   role             = aws_iam_role.lambda_role.arn
+  runtime          = "provided.al2" # this runtime work with our built lambda (not provided.al2023)
+  architectures    = ["arm64"]
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-  runtime          = "go1.x"
   source_code_hash = data.archive_file.disconnect.output_base64sha256
 
 
@@ -119,17 +122,18 @@ data "archive_file" "ws_chat" {
   depends_on = [null_resource.go_build]
 
   type        = "zip"
-  source_file = "../dist/wschat"
+  source_dir  = "../dist/wschat"
   output_path = "../dist/wschat.zip"
 }
 
 resource "aws_lambda_function" "ws_chat" {
   function_name    = "blinders-ws-chat"
   filename         = "../dist/wschat.zip"
-  handler          = "wschat"
+  handler          = "bootstrap" # default for provided.al2
   role             = aws_iam_role.lambda_role.arn
+  runtime          = "provided.al2" # this runtime work with our built lambda (not provided.al2023)
+  architectures    = ["arm64"]
   depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
-  runtime          = "go1.x"
   source_code_hash = data.archive_file.ws_chat.output_base64sha256
 
 
--- scripts/build_golambda.sh
+++ scripts/build_golambda.sh
@@ -10,21 +10,21 @@
 rm -rf dist
 echo "cleaned dist directory"
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/ ./functions/translate
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/translate/bootstrap ./functions/translate
 echo "build translate lambda function completed"
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/ ./functions/websocket/connect
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/connect/bootstrap ./functions/websocket/connect
 echo "build connect lambda function completed"
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/authorizer/handler ./functions/websocket/authorizer
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/authorizer/bootstrap ./functions/websocket/authorizer
 echo "build authorizer lambda function completed"
 cp ./firebase.admin.json ./dist/authorizer
 echo "copied firebase.admin.json to authorizer"
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/ ./functions/websocket/disconnect
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/disconnect/bootstrap ./functions/websocket/disconnect
 echo "build disconnect lambda function completed"
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/wschat ./functions/websocket/chat
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ./dist/wschat/bootstrap ./functions/websocket/chat
 echo "build websocket chat lambda function completed"
 
 # migrate to arm64 for better price-performance
