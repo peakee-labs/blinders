5f9af7e34e9f17ffcdf831ba84b554d7567d712c
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-24 21:40:07 +0700

    feat: python embeder only expose API, hide db logic in embedder

--- services/explore/blinders/explore_service/core/main.py
+++ services/explore/blinders/explore_service/core/main.py
@@ -1,68 +1,103 @@
-import os
+from fastapi import APIRouter
+from pydantic import BaseModel
 
-import redis
+from blinders.explore_core.main import Explore, MatchInfo
 
-from blinders.explore_core.main import Explore
-from blinders.explore_core.types import MatchInfo
+
+def health_check_handler():
+    return {"Status": "Service Healthy"}
+
+
+class ExplorePostBody(BaseModel):
+    name: str
+    gender: str
+    major: str
+    native: str
+    country: str
+    learnings: list[str]
+    interests: list[str]
+    age: int
 
 
 class ServiceWorker(object):
-    redis_client: redis.Redis
+    # redis_client: redis.Redis
     core: Explore
+    router: APIRouter
 
-    def __init__(self, redis_client: redis.Redis, explore_core: Explore) -> None:
-        self.redis_client = redis_client
+    # def __init__(self, redis_client: redis.Redis, explore_core: Explore) -> None:
+    def __init__(self, explore_core: Explore) -> None:
+        # self.redis_client = redis_client
         self.core = explore_core
-        self.init_redis_group()
+        self.router = APIRouter()
+        # self.init_redis_group()
 
-    def init_redis_group(self):
-        try:
-            res = self.redis_client.xgroup_create(
-                "match:embed", "matchGroup", "$", mkstream=True
-            )
-            print(res)
-        except Exception as e:
-            print(e)
-            pass
+    def init_route(self) -> None:
+        self.router.add_api_route("/ping", health_check_handler, methods=["GET"])
+        self.router.add_api_route("/explore/embed", health_check_handler, methods=["GET"])
 
-    def loop(self):
-        consumer_name = os.getenv("REDIS_CONSUMER_NAME", "default")
-        print("listening to stream, consumer name: ", consumer_name)
-        while True:
-            entries = self.redis_client.xreadgroup(
-                "matchGroup",
-                consumer_name,
-                {"match:embed": ">"},
-                block=1000,
-                count=1,
-            )
-            if not isinstance(entries, list):
-                print("reply  with unexpected format")
-                print(entries)
-                continue
+    def embed_explore_handler(self, body: ExplorePostBody):
+        matchInfor = MatchInfo(
+            userId="",
+            name=body.name,
+            gender=body.gender,
+            major=body.major,
+            native=body.native,
+            country=body.country,
+            learnings=body.learnings,
+            interests=body.interests,
+            age=body.age
+        )
+        embed = self.core.add_user_embed(matchInfor)
+        return embed
 
-            if entries is None or len(entries) == 0:
-                continue
+    # def init_redis_group(self):
+    #     try:
+    #         res = self.redis_client.xgroup_create(
+    #             "match:embed", "matchGroup", "$", mkstream=True
+    #         )
+    #         print(res)
+    #     except Exception as e:
+    #         print(e)
+    #         pass
 
-            user_id = entries[0][1][0][1]["id"]  # type: str
-            if not isinstance(user_id, str):
-                print("could not found id in stream entry")
-                continue
-
-            doc = self.core.match_col.find_one({"firebaseUID": user_id})
-            if doc is None:
-                print("user not found")
-                return
-
-            info = MatchInfo(
-                userId=doc.get("userId"),
-                name=doc.get("name"),
-                gender=doc.get("gender"),
-                major=doc.get("major"),
-                native=doc.get("native"),
-                country=doc.get("country"),
-                learnings=doc.get("learnings"),
-                interests=doc.get("interests"),
-                age=doc.get("age"),
-            )
-            self.core.add_user_embed(info)
+    # def loop(self):
+    #     consumer_name = os.getenv("REDIS_CONSUMER_NAME", "default")
+    #     print("listening to stream, consumer name: ", consumer_name)
+    #     while True:
+    #         entries = self.redis_client.xreadgroup(
+    #             "matchGroup",
+    #             consumer_name,
+    #             {"match:embed": ">"},
+    #             block=1000,
+    #             count=1,
+    #         )
+    #         if not isinstance(entries, list):
+    #             print("reply  with unexpected format")
+    #             print(entries)
+    #             continue
+    #
+    #         if entries is None or len(entries) == 0:
+    #             continue
+    #
+    #         user_id = entries[0][1][0][1]["id"]  # type: str
+    #         if not isinstance(user_id, str):
+    #             print("could not found id in stream entry")
+    #             continue
+    #
+    #         doc = self.core.match_col.find_one({"firebaseUID": user_id})
+    #         if doc is None:
+    #             print("user not found")
+    #             return
+    #
+    #         info = MatchInfo(
+    #             userId=doc.get("userId"),
+    #             name=doc.get("name"),
+    #             gender=doc.get("gender"),
+    #             major=doc.get("major"),
+    #             native=doc.get("native"),
+    #             country=doc.get("country"),
+    #             learnings=doc.get("learnings"),
+    #             interests=doc.get("interests"),
+    #             age=doc.get("age"),
+    #         )
+    #         self.core.add_user_embed(info)
--- services/explore/blinders/explore_service/main.py
+++ services/explore/blinders/explore_service/main.py
@@ -1,33 +1,35 @@
-import os
-
 import dotenv
-import pymongo
-import redis
+import uvicorn
+from fastapi import FastAPI
 
 from blinders.explore_core.embedder import Embedder
 from blinders.explore_core.main import Explore
 from blinders.explore_service.core.main import ServiceWorker
 
-matchColName = "matches"
+# matchColName = "matches"
 if __name__ == "__main__":
     dotenv.load_dotenv()
     try:
-        mongoURL = "mongodb://{}:{}@{}:{}/{}".format(
-            os.getenv("MONGO_USERNAME"),
-            os.getenv("MONGO_PASSWORD"),
-            os.getenv("MONGO_HOST"),
-            os.getenv("MONGO_PORT"),
-            os.getenv("MONGO_DATABASE"),
-        )
-        mongoClient = pymongo.MongoClient(mongoURL)
-        db = mongoClient[os.getenv("MONGO_DATABASE", "Default")]
-        matchCol = db[matchColName]
-
+        # mongoURL = "mongodb://{}:{}@{}:{}/{}".format(
+        #     os.getenv("MONGO_USERNAME"),
+        #     os.getenv("MONGO_PASSWORD"),
+        #     os.getenv("MONGO_HOST"),
+        #     os.getenv("MONGO_PORT"),
+        #     os.getenv("MONGO_DATABASE"),
+        # )
+        # mongoClient = pymongo.MongoClient(mongoURL)
+        # db = mongoClient[os.getenv("MONGO_DATABASE", "Default")]
+        # matchCol = db[matchColName]
+        #
         embedder = Embedder()
-        redis_client = redis.client.Redis(host="localhost", port=6379, decode_responses=True)
-        explore = Explore(redis_client, embedder, matchCol)
-        service_core = ServiceWorker(redis_client=redis_client, explore_core=explore)
-        service_core.loop()
+        explore = Explore(embedder)
+        # redis_client = redis.client.Redis(host="localhost", port=6379, decode_responses=True)
+        # explore = Explore(redis_client, embedder, matchCol)
+        service_core = ServiceWorker(explore_core=explore)
+        service_core.init_route()
+        app = FastAPI()
+        app.include_router(service_core.router)
+        uvicorn.run(app, host="0.0.0.0", port=8084)
 
     except Exception as e:
         print("exception: ", e)
