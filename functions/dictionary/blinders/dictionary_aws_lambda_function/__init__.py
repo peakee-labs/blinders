292c09312833139ba1d31bd5c41f14c683ea254d
Dat Nguyen <84390363+hnimtadd@users.noreply.github.com>
2024-04-01 23:01:14 +0700

    remove redundant codes

--- functions/dictionary/blinders/dictionary_aws_lambda_function/__init__.py
+++ functions/dictionary/blinders/dictionary_aws_lambda_function/__init__.py
@@ -10,7 +10,5 @@ def call_handle_dictionary():
 
 def lambda_handler(event, context):
     """Example of calling a function from another module."""
-    print(json.dumps(event, indent=4), "<-- event")
-    print(context, "<-- context")
     print("dictionary on event", event["resource"])
     return {"statusCode": 200, "body": call_handle_dictionary()}
