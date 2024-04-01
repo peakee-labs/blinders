854eddec0d63b4136d51337dfb16f56ef4a5084d
Tan Le <tanlethanh8266@gmail.com>
2024-04-01 21:35:38 +0700

    feat: implement suggest lambda with python

--- functions/dictionary/blinders/dictionary_aws_lambda_function/__init__.py
+++ functions/dictionary/blinders/dictionary_aws_lambda_function/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from blinders.dictionary_core import handle_dictionary
 
 
@@ -8,5 +10,7 @@ def call_handle_dictionary():
 
 def lambda_handler(event, context):
     """Example of calling a function from another module."""
+    print(json.dumps(event, indent=4), "<-- event")
+    print(context, "<-- context")
     print("dictionary on event", event["resource"])
     return {"statusCode": 200, "body": call_handle_dictionary()}
