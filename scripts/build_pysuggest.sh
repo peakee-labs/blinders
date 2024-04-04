0cec2e5128ddf60caedbdf99595e5ba364018077
Tan Le <tanlethanh8266@gmail.com>
2024-04-04 15:09:20 +0700

    fix: build_pysuggest with cleaning build folders

--- scripts/build_pysuggest.sh
+++ scripts/build_pysuggest.sh
@@ -1,4 +1,22 @@
+suggest_bundle="functions/suggest/lambda_bundle"
+suggest_bundle_zip="functions/suggest/lambda_bundle.zip"
+
+if [ -d "$suggest_bundle" ]; then
+    echo "Cleaning build directory..."
+    rm -rf functions/suggest/lambda_bundle
+fi
+
+if [ -f "$suggest_bundle_zip" ]; then
+    echo "Cleaning zip file..."
+    rm functions/suggest/lambda_bundle.zip
+fi
+
 DOCKER_BUILDKIT=1 docker build -t blinders-pysuggest-lambda . -f Dockerfile.build-pylambda -o ./functions/suggest/lambda_bundle
 
-cd functions/suggest/lambda_bundle
-zip -r ../lambda_bundle.zip . -x '*.pyc'
+if [ -d "$suggest_bundle" ]; then
+    echo "Suggest bundle exists"
+    cd $suggest_bundle
+    zip -r ../lambda_bundle.zip . -x '*.pyc'
+else
+    echo "Directory does not exist, build failed!"
+fi
