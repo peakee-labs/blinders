0dbf3652601cec27c05e262160a9f820b1744bee
Tan Le <tanlethanh8266@gmail.com>
2024-04-04 23:07:35 +0700

    chore: apply cors for suggest api

--- functions/suggest/blinders/suggest_lambda/__init__.py
+++ functions/suggest/blinders/suggest_lambda/__init__.py
@@ -6,6 +6,10 @@ from blinders.pysuggest import explain_text_in_sentence_by_gpt
 request_types = ["explain-text-in-sentence"]
 models = ["gpt"]
 
+default_headers = {
+    "Access-Control-Allow-Origin": "*",
+}
+
 
 def lambda_handler(event: Dict[str, Any], context):
     """Example of calling a function from another module."""
@@ -13,12 +17,13 @@ def lambda_handler(event: Dict[str, Any], context):
     print("handle suggest with payload", queries)
 
     if queries is None:
-        return {"statusCode": 400, "body": "require queries"}
+        return {"statusCode": 400, "body": "require queries", "headers": default_headers}
 
     tp = queries.get("type")
     if tp not in request_types:
         return {
             "statusCode": 400,
+            "headers": default_headers,
             "body": "unsupported type, expect" + str(request_types),
         }
 
@@ -36,8 +41,9 @@ def lambda_handler(event: Dict[str, Any], context):
             if text is None or sentence is None:
                 return {
                     "statusCode": 400,
+                    "headers": default_headers,
                     "body": "text and sentence are required for gpt",
                 }
             suggest = explain_text_in_sentence_by_gpt(text, sentence)
             print("suggest", suggest)
-            return {"statusCode": 200, "body": json.dumps(suggest)}
+            return {"statusCode": 200, "headers": default_headers, "body": json.dumps(suggest)}
