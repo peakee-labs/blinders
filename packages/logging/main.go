7c27189d9cefaf85c8e86260d4ace5b7335bf3ad
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-11 22:16:55 +0700

    refactor: move types to types.go

--- packages/logging/main.go
+++ packages/logging/main.go
@@ -4,38 +4,15 @@ import (
 	"context"
 	"time"
 
-	"blinders/packages/transport"
-
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
-	EventTypeSuggestLanguageUnit transport.EventType = "SUGGEST_LANGUAGE_UNIT"
-	LogCollection                                    = "logs"
-)
-
-type (
-	LanguageUnitRequest struct {
-		Text     string `json:"text" bson:"text"`
-		Sentence string `json:"sentence" bson:"sentence"`
-	} // TODO: we could put this struct in another pkg
-	LanguageUnitResponse struct {
-		Translate       string         `json:"translate" bson:"translate"`
-		GrammarAnalysis map[string]any `json:"grammarAnalysis" bson:"grammarAnalysis"`
-		ExpandWords     []string       `json:"expandWords" bson:"expandWords"`
-	} // TODO: we could put this struct in another pkg
-	SuggestLanguageUnitEvent struct {
-		UserID   string               `json:"userId" bson:"userId"`
-		Request  LanguageUnitRequest  `json:"request" bson:"request"`
-		Response LanguageUnitResponse `json:"response" bson:"response"`
-	} // TODO: we could put this struct in another pkg
-	TranslateEventLog struct {
-		SuggestLanguageUnitEvent `json:",inline" bson:",inline"`
-		ID                       primitive.ObjectID `json:"logId" bson:"_id"`
-		CreatedAt                primitive.DateTime `json:"createdAt" bson:"createdAt"`
-	}
+	EventTypeSuggestPracticeUnit EventType = "SUGGEST_LANGUAGE_UNIT"
+	EventTypeTranslate           EventType = "TRANSLATE"
+	LogCollection                          = "logs"
 )
 
 type EventLogger struct {
@@ -75,4 +52,68 @@ func (l EventLogger) GetTranslateLogByID(logID primitive.ObjectID) (*TranslateEv
 	err := l.Col.FindOne(ctx, filter).Decode(translateLog)
 
 	return translateLog, err
+}
+
+// GetTranslateLogByUserID returns list of translate log belongs to user with ID userID.
+func (l EventLogger) GetTranslateLogByUserID(userID primitive.ObjectID) ([]TranslateEventLog, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	filter := bson.M{"userID": userID}
+
+	cur, err := l.Col.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []TranslateEventLog
+	if err := cur.All(ctx, res); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// AddSuggestPracticeLog adds suggest practice unit log into db.
+func (l EventLogger) AddSuggestPracticeLog(log *SuggestPracticeUnitEventLog) (*SuggestPracticeUnitEventLog, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	_, err := l.Col.InsertOne(ctx, log)
+	return log, err
+}
+
+// AddRawSuggestPracticeUnitLog assigns unique primitive.ObjectID and primitive.DateTime to log then pass
+// to EventLogger.AddSuggestPracticeLog method
+func (l EventLogger) AddRawSuggestPracticeUnitLog(log *SuggestPracticeUnitEventLog) (*SuggestPracticeUnitEventLog, error) {
+	log.ID = primitive.NewObjectID()
+	log.CreatedAt = primitive.NewDateTimeFromTime(time.Now())
+	return l.AddSuggestPracticeLog(log)
+}
+
+// GetSuggestPracticeUnitLogByID returns suggest practice unit with SuggestPracticeUnitEventLog.ID equal to params.logID in db.
+func (l EventLogger) GetSuggestPracticeUnitLogByID(logID primitive.ObjectID) (*SuggestPracticeUnitEventLog, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	filter := bson.M{"_id": logID}
+	translateLog := new(SuggestPracticeUnitEventLog)
+	err := l.Col.FindOne(ctx, filter).Decode(translateLog)
+
+	return translateLog, err
+}
+
+// GetSuggestPracticeUnitEventLogByUserID returns suggest practice unit logs belong to user with userID
+func (l EventLogger) GetSuggestPracticeUnitEventLogByUserID(userID primitive.ObjectID) ([]SuggestPracticeUnitEventLog, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	filter := bson.M{"userID": userID}
+
+	cur, err := l.Col.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var res []SuggestPracticeUnitEventLog
+	if err := cur.All(ctx, res); err != nil {
+		return nil, err
+	}
+	return res, nil
 }
