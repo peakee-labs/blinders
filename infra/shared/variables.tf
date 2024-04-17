91118f1c3da28181565fdae1c2c4ad0a3af21058
Tan Le <tanlethanh8266@gmail.com>
2024-04-17 18:14:17 +0700

    feat: shared infra state with route53 validation

---
+++ infra/shared/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "admin.peakee"
+}
+
+variable "aws_route53_zone_name" {
+  type    = string
+  default = "peakee.co"
+}
+
+variable "domain_name_for_certificate" {
+  type    = string
+  default = "peakee.co"
+}
+
+variable "subject_alternative_names_for_certificate" {
+  type    = list(string)
+  default = ["api.peakee.co", "*.api.peakee.co", "ws.peakee.co", "*.ws.peakee.co"]
+}
+
