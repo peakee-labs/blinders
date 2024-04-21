81e5838f45e11a34cfb10bd1319167492b27efda
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-22 01:42:35 +0700

    chore: fix lints

--- packages/pytransport/blinders/pytransport/__init__.py
+++ packages/pytransport/blinders/pytransport/__init__.py
@@ -2,5 +2,5 @@ class ITransport:
     def Request(self, id: str, payload: str) -> str:
         raise Exception("this method is not implemented")
 
-    def Push(self, id: str, payload: str):
+    def Push(self, id: str, payload: str) -> None:
         raise Exception("this method is not implemented")
