64c816af92a5f5722b0332425015fd4111482eba
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-21 21:44:42 +0700

    refactor: use transport request to communicate with collecting endpoint

--- services/practice/api/handlers.go
+++ services/practice/api/handlers.go
@@ -21,33 +21,45 @@ func (s Service) HandleSuggestLanguageUnit(ctx *fiber.Ctx) error {
 		return fmt.Errorf("cannot get user auth information")
 	}
 
-	var unit collecting.SuggestPracticeUnitResponse
-	transportBody := map[string]string{
-		"userID": authUser.ID,
-	}
-	transportBytes, _ := json.Marshal(transportBody)
+	var (
+		req = transport.GetEventRequest{
+			Request:   transport.Request{Type: transport.GetEvent},
+			UserID:    authUser.ID,
+			NumReturn: 1,
+			Type:      collecting.EventTypeSuggestPracticeUnit,
+		}
+		rsp = new(transport.GetEventResponse)
+	)
 
-	response, err := s.Transport.Request(ctx.Context(), s.ConsumerMap[transport.Collecting], transportBytes)
+	transportBytes, _ := json.Marshal(req)
+
+	response, err := s.Transport.Request(
+		ctx.Context(),
+		s.ConsumerMap[transport.Collecting],
+		transportBytes,
+	)
 	if err != nil {
 		log.Printf("practice: cannot get log event from collecting service, err: %v\n", err)
-
-		preferedLang := ctx.Query("lang")
-		if preferedLang != "" {
-			unit, err = s.GetRandomPracticeUnitWithLangCode(preferedLang)
-			if err == nil {
-				goto response
-			}
-			log.Printf("practice: cannot get predefined event, lang: %v, err: %v\n", preferedLang, err)
-		}
-		return ctx.SendStatus(fiber.StatusBadRequest)
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot get practice unit"})
 	}
 
-	if err := json.Unmarshal(response, &unit); err != nil {
+	if err := json.Unmarshal(response, &rsp); err != nil {
 		log.Printf("practice: cannot parse result from collecting service, err: %v\n", err)
 	}
+	if len(rsp.Data) == 0 {
+		log.Printf("practice: response includes no event: \n")
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot get practice unit"})
+	}
 
-response:
-	return ctx.Status(fiber.StatusOK).JSON(unit)
+	event := rsp.Data[0]
+	switch event.Type {
+	case collecting.EventTypeSuggestPracticeUnit:
+		return ctx.Status(fiber.StatusOK).JSON(event.Payload)
+
+	default:
+		log.Printf("practice: unsupported event type (%v)\n", event.Type)
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot get practice unit"})
+	}
 }
 
 func (s Service) HandleGetRandomLanguageUnit(ctx *fiber.Ctx) error {
