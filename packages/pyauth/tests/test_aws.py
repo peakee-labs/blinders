80c365ee27f5d7f5f8769f344a4dafcb6ee2199b
Dat Nguyen <minhdat15012002@gmail.com>
2024-04-27 15:20:51 +0700

    feat: aws auth middleware python implementation

---
+++ packages/pyauth/tests/test_aws.py
@@ -0,0 +1,39 @@
+import json
+from typing import Dict, Any
+
+from blinders.pyauth import AuthManager
+from blinders.pydb import MongoManager
+from blinders.pydb.utils.mongo import init_mongo_client
+from blinders.pydb.types import User
+from blinders.pyauth import AuthUser
+from blinders.pyauth.aws import auth
+
+
+testURL = "mongodb://localhost:27017"
+testDB = "blinders-test"
+
+
+def test_aws():
+    auth_cert: Dict[str, Any] = {}
+    with open("firebase.admin.json") as file:
+        auth_cert = json.load(file)
+
+    auth_manager = AuthManager(auth_cert)
+    valid_firebaseuid = "t7ZYtyjYCbMxOefUALu8b2P4AVO2"
+    user = User(
+        name="name", firebaseUID=valid_firebaseuid, email="email", friends=[], imageURL="imageURL"
+    )
+    mongo_manager = MongoManager(
+        client=init_mongo_client(testURL),
+        name=testDB,
+    )
+
+    added_user = mongo_manager.UsersRepo.insert_raw_user(user=user)
+    mockEvent = {"headers": {"authorization": "Bearer sampletoken"}}
+
+    def test_handler(auth_user: AuthUser, event=mockEvent):
+        assert auth_user.get("AuthID") == valid_firebaseuid
+        assert auth_user.get("ID") == str(added_user.get("_id"))
+
+    auth(auth_manager=auth_manager, repo=mongo_manager.UsersRepo)(test_handler)(event=mockEvent)
+    mongo_manager.UsersRepo.Col.drop()
