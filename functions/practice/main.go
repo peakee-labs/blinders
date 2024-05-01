5315f228d09228191c54a1171042b1794b3a06e0
Tan Le <tanlethanh8266@gmail.com>
2024-05-01 22:49:42 +0700

    refactor: correct lambda functions which are communicating with collecting service

--- functions/practice/main.go
+++ functions/practice/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
 	"blinders/packages/auth"
-	"blinders/packages/db"
+	"blinders/packages/db/usersdb"
+	dbutils "blinders/packages/db/utils"
 	"blinders/packages/transport"
 	"blinders/packages/utils"
 	practiceapi "blinders/services/practice/api"
@@ -21,28 +21,13 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-var (
-	fiberLambda    *fiberadapter.FiberLambda
-	authMiddleware auth.LambdaMiddleware
-)
+var fiberLambda *fiberadapter.FiberLambda
 
 func init() {
 	log.Println("practice api running on environment:", os.Getenv("ENVIRONMENT"))
-	app := fiber.New(fiber.Config{})
 
-	url := fmt.Sprintf(
-		db.MongoURLTemplate,
-		os.Getenv("MONGO_USERNAME"),
-		os.Getenv("MONGO_PASSWORD"),
-		os.Getenv("MONGO_HOST"),
-		os.Getenv("MONGO_PORT"),
-		os.Getenv("MONGO_DATABASE"),
-	)
-
-	database := db.NewMongoManager(url, os.Getenv("MONGO_DATABASE"))
-	if database == nil {
-		log.Fatal("cannot create database manager")
-	}
+	usersDB, err := dbutils.InitMongoDatabaseFromEnv("USERS")
+	usersRepo := usersdb.NewUsersRepo(usersDB)
 
 	adminConfig, err := utils.GetFile("firebase.admin.json")
 	if err != nil {
@@ -58,30 +43,27 @@ func init() {
 	if err != nil {
 		log.Fatal("failed to load aws config", err)
 	}
+
+	app := fiber.New(fiber.Config{})
 	api := practiceapi.NewService(
 		app,
 		authManager,
-		database,
+		usersRepo,
 		transport.NewLambdaTransport(cfg),
 		transport.ConsumerMap{
 			transport.CollectingPush: os.Getenv("COLLECTING_PUSH_FUNCTION_NAME"),
 			transport.CollectingGet:  os.Getenv("COLLECTING_GET_FUNCTION_NAME"),
 		},
 	)
-	api.App.Use(logger.New(logger.Config{
-		Format: "${time} | ${status} | ${latency} | ${ip} | ${method} | ${path} | ${queryParams} | ${error}\n",
-	}))
-
+	api.App.Use(logger.New(logger.Config{Format: utils.DefaultGinLoggerFormat}))
 	api.App.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
+		AllowOrigins: utils.GetOriginsFromEnv(),
 		AllowMethods: "GET",
 		AllowHeaders: "*",
 	}))
-
 	api.InitRoute()
 
 	fiberLambda = fiberadapter.New(api.App)
-	authMiddleware = auth.LambdaAuthMiddleware(authManager, database.Users)
 }
 
 func HandleRequest(
@@ -92,5 +74,5 @@ func HandleRequest(
 }
 
 func main() {
-	lambda.Start(authMiddleware(HandleRequest))
+	lambda.Start((HandleRequest))
 }
