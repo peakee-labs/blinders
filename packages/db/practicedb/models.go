6d302e2b845ed0ffcaec2ff72dca8be092c66967
Dat Nguyen <minhdat15012002@gmail.com>
2024-05-25 10:55:41 +0700

    chore: resolve lint

--- packages/db/practicedb/models.go
+++ packages/db/practicedb/models.go
@@ -5,9 +5,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type FlashCard struct {
 	ID           primitive.ObjectID `json:"id" bson:"_id"`
 	FrontText    string             `json:"frontText" bson:"frontText"`
-	FrontImgUrl  string             `json:"frontImageUrl"`
+	FrontImgURL  string             `json:"frontImageUrl"`
 	BackText     string             `json:"backText" bson:"backText"`
-	BackImgUrl   string             `json:"backImageURL" bson:"backImageURL"`
+	BackImgURL   string             `json:"backImageURL" bson:"backImageURL"`
 	UserID       primitive.ObjectID `json:"userId" bson:"userId"`
 	CollectionID primitive.ObjectID `json:"collectionId" bson:"collectionId"`
 }
