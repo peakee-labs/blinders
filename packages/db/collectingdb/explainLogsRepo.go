ae89d59a3b4547905b4bf54a037fbad87fadcefd
Dat Nguyen <minhdat15012002@gmail.com>
2024-06-02 17:19:46 +0700

    feat: support get explain log with timebased pagination

--- packages/db/collectingdb/explainLogsRepo.go
+++ packages/db/collectingdb/explainLogsRepo.go
@@ -49,3 +49,54 @@ func (r ExplainLogsRepo) GetLogWithSmallestGetCountByUserID(
 
 	return &elog, nil
 }
+
+type Pagination struct {
+	From  time.Time `json:"from_time"`
+	To    time.Time `json:"to_time"`
+	Limit int       `json:"limit"`
+}
+
+func (r ExplainLogsRepo) GetLogWithPagination(
+	userID primitive.ObjectID,
+	opt ...Pagination,
+) ([]*ExplainLog, *Pagination, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	var pipeline []bson.M
+
+	if len(opt) > 0 {
+		option := opt[0]
+		pipeline = []bson.M{
+			{"$match": bson.M{
+				"userId": userID,
+				"createdAt": bson.M{
+					"$gte": primitive.NewDateTimeFromTime(option.From),
+					"$lte": primitive.NewDateTimeFromTime(option.To)},
+			},
+			},
+			{"$sort": bson.M{"createdAt": 1}},
+			{"$limit": option.Limit},
+		}
+	} else {
+		pipeline = []bson.M{
+			{"$match": bson.M{"userId": userID}},
+			{"$sort": bson.M{"createdAt": 1}},
+		}
+	}
+
+	cursor, err := r.Collection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, nil, fmt.Errorf("can not get explain log")
+	}
+
+	var logs []*ExplainLog
+	if err = cursor.All(ctx, &logs); err != nil {
+		return nil, nil, fmt.Errorf("can not get explain log")
+	}
+
+	return logs, &Pagination{
+		From:  logs[0].CreatedAt.Time(),
+		To:    logs[len(logs)-1].CreatedAt.Time(),
+		Limit: len(logs),
+	}, nil
+}
