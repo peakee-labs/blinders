866889e76a5fcf914c06fae3b58b1c3de332f260
Tan Le <tanlethanh8266@gmail.com>
2024-07-15 15:25:25 +0700

    feat: add mono server entry

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module blinders/server
+
+go 1.22.1
---
+++ server/main.go
@@ -0,0 +1,14 @@
+/*
+Run all services as a monolith server
+*/
+package main
+
+import "log"
+
+func init() {
+	log.Println("run init mono")
+}
+
+func main() {
+	log.Println("run main mono")
+}
